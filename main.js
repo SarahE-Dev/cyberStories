226883fb623c3088d92cb381bd82dbcfc0d0b67f
Sarah E <seatherly.prsvr@gmail.com>
2023-10-17 14:48:02 -0500

    promise all fix

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
 
-let idArr = [];
+
 let storyObjects = [];
 let allComments = [];
 
 fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
     .then((data) => data.json())
     .then((storyIds) => {
-        idArr = storyIds
-        idArr.length = 102
-        return idArr 
+        const idArr = []
+        storyIds.length = 100
+        for(let id of storyIds){
+            const request = fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+            idArr.push(request)
+        }
+        return Promise.all(idArr)
     })
-    .then((idArr) => {
-        for(let i = 0; i < idArr.length; i++){
-            fetch(`https://hacker-news.firebaseio.com/v0/item/${idArr[i]}.json?print=pretty`)
-            .then((data) => data.json())
-            .then((story) => {
-                let link = story.url
+    .then(responseArr =>{
+        const requestArr = []
+        for(let res of responseArr){
+            requestArr.push(res.json())
+        }
+        return Promise.all(requestArr)
+    })
+    .then(data=>{
+        let commentIds = [];
+        for(let object of data){
+            htmlElementMaker(object)
+            commentIds.push(object.kids)
+        }
+        console.log(commentIds[0])
+    })
+    
+    
+    
+    function htmlElementMaker(story){
+        let link = story.url
                 if(link === undefined){
                     link = '';
                 }
@@ -24,7 +42,7 @@ fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
                 if(numOfComments === undefined){
                     numOfComments = '0';
                 }
-                let html = `<li class="bg-gradient list-group-item d-flex justify-content-between align-items-start" id="list${i}">
+                let html = `<li class="bg-gradient list-group-item d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto small">
                     <div class="fw-bold small">${story.title}<a href="${story.url}" class="small badge link-secondary">${linkShown[2]}</a></div>
                     <p class="badge">${story.score} points   |   Submitted by: ${story.by}   |   ${numOfComments} comments |  <a id="${story.id}" class="small link-danger comments">View comments</a></p>
@@ -32,22 +50,7 @@ fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
                   <span class="badge bg-secondary rounded-pill">${story.score}</span>
             </li>`
                $('ol').append(html)
-               console.log(story.kids)
-               let commentIds = story.kids
-               allComments.push(commentIds)
-            })
-        
-        
-        }
-        return allComments
-    })
-    
-    
-    console.log(allComments[0])
-    console.log(typeof(allComments))
-    
-    
-    
+    }
 
 
 
