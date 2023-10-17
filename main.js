4b9d25216fb1e46482e1e59d41cc2c1d58a767a3
Sarah E <seatherly.prsvr@gmail.com>
2023-10-17 14:17:42 -0500

    edit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 let idArr = [];
 let storyObjects = [];
 let allComments = [];
+
 fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
     .then((data) => data.json())
     .then((storyIds) => {
@@ -15,28 +16,37 @@ fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
             .then((data) => data.json())
             .then((story) => {
                 let link = story.url
-                let shown = link.split('/')
-                let comments = story.descendants
-                if(comments === undefined){
-                    comments = '0';
+                if(link === undefined){
+                    link = '';
+                }
+                let linkShown = link.split('/')
+                let numOfComments = story.descendants
+                if(numOfComments === undefined){
+                    numOfComments = '0';
                 }
                 let html = `<li class="bg-gradient list-group-item d-flex justify-content-between align-items-start" id="list${i}">
                 <div class="ms-2 me-auto small">
-                    <div class="fw-bold small">${story.title}<a href="${story.url}" class="small badge link-secondary">${shown[2]}</a></div>
-                    <p class="badge">${story.score} points   |   Submitted by: ${story.by}   |   ${comments} comments |  <a id="${story.id}" class="small link-danger comments">View comments</a></p>
+                    <div class="fw-bold small">${story.title}<a href="${story.url}" class="small badge link-secondary">${linkShown[2]}</a></div>
+                    <p class="badge">${story.score} points   |   Submitted by: ${story.by}   |   ${numOfComments} comments |  <a id="${story.id}" class="small link-danger comments">View comments</a></p>
                   </div>
                   <span class="badge bg-secondary rounded-pill">${story.score}</span>
             </li>`
-               $('ol').append(html) 
-               allComments.push(story.kids)
-               
-               return allComments
-                })
+               $('ol').append(html)
+               console.log(story.kids)
+               let commentIds = story.kids
+               allComments.push(commentIds)
+            })
         
-            }
-           
         
+        }
+        return allComments
     })
+    
+    
+    console.log(allComments[0])
+    console.log(typeof(allComments))
+    
+    
     
 
 
